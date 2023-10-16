1bc8f4e6d107666050fa00deb0d0eefef38e89ba
Da Silva Maria Leidinice <mda_silv@login02.hpc>
2023-10-16 16:55:44 +0200

    update scripts

--- sam_3km/plot_maps_study_area_sa.py
+++ sam_3km/plot_maps_study_area_sa.py
@@ -17,9 +17,10 @@ from matplotlib.patches import Polygon
 from matplotlib.patches import PathPatch
 from mpl_toolkits.basemap import Basemap, cm
 
+    
 # Specify directories 
 dirnc = '/home/mda_silv/scratch/sam_3km/input'
-domname = 'sam-3km-pbl1'
+domname = 'SAM-3km'
 
 # RegCM file
 if len(sys.argv) > 1:
@@ -35,42 +36,30 @@ latc = RCMf.latitude_of_projection_origin
 RCMf.close()
 
 lat_start = -58
-lat_end   = 14
-lon_start = -85
+lat_end   = 18
+lon_start = -90
 lon_end   = -30
 
 # Plot study area
 fig = plt.figure() 
 font_size = 10
 
-my_map = Basemap(llcrnrlon=lon_start, llcrnrlat=lat_start, urcrnrlon=lon_end, urcrnrlat=lat_end, resolution='l')	
-my_map.drawparallels(np.arange(lat_start, lat_end, 8.), labels=[1,0,0,0], fontsize=font_size, linewidth=0.4, color='black')
+my_map = Basemap(llcrnrlon=lon_start, llcrnrlat=lat_start, urcrnrlon=lon_end, urcrnrlat=lat_end, resolution='c')	
+my_map.drawparallels(np.arange(lat_start, lat_end, 10.), labels=[1,0,0,0], fontsize=font_size, linewidth=0.4, color='black')
 my_map.drawmeridians(np.arange(lon_start, lon_end, 10.), labels=[0,0,0,1], fontsize=font_size, linewidth=0.4, color='black')                  
-my_map.readshapefile('/home/mda_silv/github_projects/shp/shp_america_sul/america_sul', 'america_sul', drawbounds=True, color='black', linewidth=1.)
+my_map.readshapefile('/home/mda_silv/github_projects/shp/shp_america_sul/america_sul', 'america_sul', drawbounds=True, color='black', linewidth=.5)
+my_map.readshapefile('/home/mda_silv/github_projects/shp/lim_unid_fed/lim_unid_fed', 'lim_unid_fed', drawbounds=True, color='black', linewidth=.5)
+	
 x, y = my_map(lon,lat)
 
-llevels = (1, 25, 50, 100, 200, 300, 400, 500, 1000, 1500, 2000, 2500, 3000)
+llevels = (0, 25, 50, 100, 200, 300, 400, 500, 1000, 1500, 2000, 2500, 3000)
 im = my_map.contourf(x, y, topo, llevels, cmap=plt.cm.terrain, extend='max')
 plt.xlabel(u'Longitude', labelpad=20, fontsize=font_size, fontweight='bold')
 plt.ylabel(u'Latitude', labelpad=30, fontsize=font_size, fontweight='bold')
 cbar = fig.colorbar(im, drawedges=True, fraction=0.030, pad=0.04, aspect=20)
 cbar.set_label('Topography (meters)', fontsize=font_size, fontweight='bold')
 
-plt.text(-36, 8, u'\u25B2 \nN', color='black', fontsize=font_size, fontweight='bold')
-
-lo1 = -79.67558
-lo2 = -35.76505
-la1 = -34.43281
-la2 = -11.05942
-
-#l1
-my_map.drawgreatcircle(lo1, la1, lo1, la2,linewidth=2,color='r')
-#l2
-my_map.drawgreatcircle(lo1, la2, lo2, la2,linewidth=2,color='r')
-#l3
-my_map.drawgreatcircle(lo2, la2, lo2, la1,linewidth=2,color='r')
-#l4
-my_map.drawgreatcircle(lo2, la1, lo1, la1,linewidth=2,color='r') 
+plt.text(-36, 11, u'\u25B2 \nN', color='black', fontsize=font_size, fontweight='bold')
 
 # Path out to save figure
 path_out = '/home/mda_silv/figs/sam_3km'
@@ -78,3 +67,4 @@ name_out = 'pyplt_maps_study_area_sam_3km.png'
 plt.savefig(os.path.join(path_out, name_out), dpi=400, bbox_inches='tight')
 plt.show()
 exit()
+
