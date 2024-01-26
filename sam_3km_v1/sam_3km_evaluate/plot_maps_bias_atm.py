535608bafb0bbdbb84915db4eb29ac0763622192
Leidinice Silva <leidinicesilva@gmail.com>
2024-01-26 14:28:49 +0100

    update scripts

--- sam_3km_v1/sam_3km_evaluate/plot_maps_bias_atm.py
+++ sam_3km_v1/sam_3km_evaluate/plot_maps_bias_atm.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 from import_climate_tools import compute_mbe
 
-var = 'uvq'
+var = 'quv'
 domain = 'SAM-3km'
 level = '200hPa'
 path='/marconi/home/userexternal/mdasilva'
@@ -22,12 +22,12 @@ path='/marconi/home/userexternal/mdasilva'
 
 def import_grid(param, level, domain, dataset, season):
 
-	arq   = '{0}/user/mdasilva/SAM-3km/post_evaluate/{1}_{2}_{3}_{4}_{5}_2018-2021_lonlat.nc'.format(path, param, level, domain, dataset, season)	
+	arq   = '{0}/user/mdasilva/SAM-3km_v1/post_evaluate/{1}_{2}_{3}_{4}_{5}_2018-2021_lonlat.nc'.format(path, param, level, domain, dataset, season)	
 	data  = netCDF4.Dataset(arq)
 	var   = data.variables[param][:] 
 	lat   = data.variables['lat'][:]
 	lon   = data.variables['lon'][:]
-	mean = var[:][:,:,:,:]
+	mean = var[:][:,0,:,:]
 	
 	return lat, lon, mean
 
@@ -85,11 +85,18 @@ mbe_mam_regcm_era5_q = compute_mbe(q_regcm_mam[0], q_era5_mam[0])
 mbe_jja_regcm_era5_q = compute_mbe(q_regcm_jja[0], q_era5_jja[0])
 mbe_son_regcm_era5_q = compute_mbe(q_regcm_son[0], q_era5_son[0])
 
-# Plot figure   
+# Plot figure 
+mulc = 1000
+lev1 = np.arange(-16, 18, 2)
+lev2 = np.arange(-16, 18, 2)
+lev3 = np.arange(-0.06, 0.07, 0.01)  
+#lev3 = np.arange(-0.8, 0.9, 0.1)  
+#lev3 = np.arange(-0.8, 0.9, 0.1)  
+
 dict_plot = {
-'u': [u'Bias of zonal wind {0} (m s⁻¹)'.format(level), np.arange(-20, 22, 2), cm.RdBu_r],
-'v': [u'Bias of meridional wind {0} (m s⁻¹)'.format(level), np.arange(-20, 22, 2), cm.RdBu_r],
-'q': [u'Bias of specific humidity {0} (kg kg⁻¹)'.format(level), np.arange(-0.02, 0.03, 0.005), cm.BrBG]
+'u': [u'Bias of zonal wind {0} (m s$^-$$^1$)'.format(level), lev1, cm.RdBu_r],
+'v': [u'Bias of meridional wind {0} (m s$^-$$^1$)'.format(level), lev2, cm.RdBu_r],
+'q': [u'Bias of specific humidity {0} (10e$^-$$^3$kg kg$^-$$^1$)'.format(level), lev3, cm.BrBG]
 }
 
 font_size = 8
@@ -97,81 +104,81 @@ fig = plt.figure(figsize=(8, 8))
 
 ax = fig.add_subplot(4, 3, 1)  
 map, xx, yy = basemap(lat, lon)
-plt_map = map.contourf(xx, yy, mbe_djf_regcm_era5_u[0], levels=dict_plot['u'][1], cmap=dict_plot['u'][2], extend='both') 
+plt_map = map.contourf(xx, yy, mbe_djf_regcm_era5_u, levels=dict_plot['u'][1], cmap=dict_plot['u'][2], extend='both') 
 plt.title(u'(a) RegCM5-ERA5 DJF', loc='left', fontsize=font_size, fontweight='bold')
 
 # Set colobar
-cbar = plt.colorbar(plt_map, cax=fig.add_axes([0.94, 0.3, 0.015, 0.4]))
+cbar = plt.colorbar(plt_map, cax=fig.add_axes([0.92, 0.3, 0.015, 0.4]))
 cbar.set_label('{0}'.format(dict_plot['u'][0]), fontsize=font_size, fontweight='bold')
 cbar.ax.tick_params(labelsize=font_size)
 
 ax = fig.add_subplot(4, 3, 2)  
 map, xx, yy = basemap(lat, lon)
-plt_map = map.contourf(xx, yy, mbe_djf_regcm_era5_v[0], levels=dict_plot['v'][1], cmap=dict_plot['v'][2], extend='both') 
+plt_map = map.contourf(xx, yy, mbe_djf_regcm_era5_v, levels=dict_plot['v'][1], cmap=dict_plot['v'][2], extend='both') 
 plt.title(u'(b) RegCM5-ERA5 DJF', loc='left', fontsize=font_size, fontweight='bold')
 
 # Set colobar
-cbar = plt.colorbar(plt_map, cax=fig.add_axes([1.05, 0.3, 0.015, 0.4]))
+cbar = plt.colorbar(plt_map, cax=fig.add_axes([1.03, 0.3, 0.015, 0.4]))
 cbar.set_label('{0}'.format(dict_plot['v'][0]), fontsize=font_size, fontweight='bold')
 cbar.ax.tick_params(labelsize=font_size)
 
 ax = fig.add_subplot(4, 3, 3)  
 map, xx, yy = basemap(lat, lon)
-plt_map = map.contourf(xx, yy, mbe_djf_regcm_era5_q[0], levels=dict_plot['q'][1], cmap=dict_plot['q'][2], extend='both') 
+plt_map = map.contourf(xx, yy, mbe_djf_regcm_era5_q*mulc, levels=dict_plot['q'][1], cmap=dict_plot['q'][2], extend='both') 
 plt.title(u'(c) RegCM5-ERA5 DJF', loc='left', fontsize=font_size, fontweight='bold')
 
 # Set colobar
-cbar = plt.colorbar(plt_map, cax=fig.add_axes([1.15, 0.3, 0.015, 0.4]))
+cbar = plt.colorbar(plt_map, cax=fig.add_axes([1.13, 0.3, 0.015, 0.4]))
 cbar.set_label('{0}'.format(dict_plot['q'][0]), fontsize=font_size, fontweight='bold')
 cbar.ax.tick_params(labelsize=font_size)
 
 ax = fig.add_subplot(4, 3, 4)  
 map, xx, yy = basemap(lat, lon)
-plt_map = map.contourf(xx, yy, mbe_mam_regcm_era5_u[0], levels=dict_plot['u'][1], cmap=dict_plot['u'][2], extend='both') 
+plt_map = map.contourf(xx, yy, mbe_mam_regcm_era5_u, levels=dict_plot['u'][1], cmap=dict_plot['u'][2], extend='both') 
 plt.title(u'(d) RegCM5-ERA5 MAM', loc='left', fontsize=font_size, fontweight='bold')
 
 ax = fig.add_subplot(4, 3, 5)  
 map, xx, yy = basemap(lat, lon)
-plt_map = map.contourf(xx, yy, mbe_mam_regcm_era5_v[0], levels=dict_plot['v'][1], cmap=dict_plot['v'][2], extend='both') 
+plt_map = map.contourf(xx, yy, mbe_mam_regcm_era5_v, levels=dict_plot['v'][1], cmap=dict_plot['v'][2], extend='both') 
 plt.title(u'(e) RegCM5-ERA5 MAM', loc='left', fontsize=font_size, fontweight='bold')
 
 ax = fig.add_subplot(4, 3, 6)  
 map, xx, yy = basemap(lat, lon)
-plt_map = map.contourf(xx, yy, mbe_mam_regcm_era5_q[0], levels=dict_plot['q'][1], cmap=dict_plot['q'][2], extend='both') 
+plt_map = map.contourf(xx, yy, mbe_mam_regcm_era5_q*mulc, levels=dict_plot['q'][1], cmap=dict_plot['q'][2], extend='both') 
 plt.title(u'(f) RegCM5-ERA5 MAM', loc='left', fontsize=font_size, fontweight='bold')
 
 ax = fig.add_subplot(4, 3, 7)  
 map, xx, yy = basemap(lat, lon)
-plt_map = map.contourf(xx, yy, mbe_jja_regcm_era5_u[0], levels=dict_plot['u'][1], cmap=dict_plot['u'][2], extend='both') 
+plt_map = map.contourf(xx, yy, mbe_jja_regcm_era5_u, levels=dict_plot['u'][1], cmap=dict_plot['u'][2], extend='both') 
 plt.title(u'(g) RegCM5-ERA5 JJA', loc='left', fontsize=font_size, fontweight='bold')
 
 ax = fig.add_subplot(4, 3, 8)  
 map, xx, yy = basemap(lat, lon)
-plt_map = map.contourf(xx, yy, mbe_jja_regcm_era5_v[0], levels=dict_plot['v'][1], cmap=dict_plot['v'][2], extend='both') 
+plt_map = map.contourf(xx, yy, mbe_jja_regcm_era5_v, levels=dict_plot['v'][1], cmap=dict_plot['v'][2], extend='both') 
 plt.title(u'(h) RegCM5-ERA5 JJA', loc='left', fontsize=font_size, fontweight='bold')
 
 ax = fig.add_subplot(4, 3, 9)  
 map, xx, yy = basemap(lat, lon)
-plt_map = map.contourf(xx, yy, mbe_jja_regcm_era5_q[0], levels=dict_plot['q'][1], cmap=dict_plot['q'][2], extend='both') 
+plt_map = map.contourf(xx, yy, mbe_jja_regcm_era5_q*mulc, levels=dict_plot['q'][1], cmap=dict_plot['q'][2], extend='both') 
 plt.title(u'(i) RegCM5-ERA5 JJA', loc='left', fontsize=font_size, fontweight='bold')
 
 ax = fig.add_subplot(4, 3, 10)  
 map, xx, yy = basemap(lat, lon)
-plt_map = map.contourf(xx, yy, mbe_son_regcm_era5_u[0], levels=dict_plot['u'][1], cmap=dict_plot['u'][2], extend='both') 
+plt_map = map.contourf(xx, yy, mbe_son_regcm_era5_u, levels=dict_plot['u'][1], cmap=dict_plot['u'][2], extend='both') 
 plt.title(u'(j) RegCM5-ERA5 SON', loc='left', fontsize=font_size, fontweight='bold')
 
 ax = fig.add_subplot(4, 3, 11)  
 map, xx, yy = basemap(lat, lon)
-plt_map = map.contourf(xx, yy, mbe_son_regcm_era5_v[0], levels=dict_plot['v'][1], cmap=dict_plot['v'][2], extend='both') 
+plt_map = map.contourf(xx, yy, mbe_son_regcm_era5_v, levels=dict_plot['v'][1], cmap=dict_plot['v'][2], extend='both') 
 plt.title(u'(k) RegCM5-ERA5 SON', loc='left', fontsize=font_size, fontweight='bold')
 
 ax = fig.add_subplot(4, 3, 12)  
 map, xx, yy = basemap(lat, lon)
-plt_map = map.contourf(xx, yy, mbe_son_regcm_era5_q[0], levels=dict_plot['q'][1], cmap=dict_plot['q'][2], extend='both') 
+plt_map = map.contourf(xx, yy, mbe_son_regcm_era5_q*mulc, levels=dict_plot['q'][1], cmap=dict_plot['q'][2], extend='both') 
 plt.title(u'(l) RegCM5-ERA5 SON', loc='left', fontsize=font_size, fontweight='bold')
 	
 # Path out to save figure
-path_out = '{0}/user/mdasilva/SAM-3km/figs'.format(path)
+path_out = '{0}/user/mdasilva/SAM-3km_v1/figs/evaluate'.format(path)
 name_out = 'pyplt_maps_bias_{0}_{1}_SAM-3km_RegCM5_2018-2021.png'.format(var, level)
 plt.savefig(os.path.join(path_out, name_out), dpi=400, bbox_inches='tight')
 plt.show()
