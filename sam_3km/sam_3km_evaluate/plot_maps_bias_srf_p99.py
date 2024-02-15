72b44736e6308a9971b70a4ebca1b24ca6a4ec14
Leidinice Silva <leidinicesilva@gmail.com>
2024-02-15 22:22:56 +0100

    update scripts

--- sam_3km/sam_3km_evaluate/plot_maps_bias_srf_p99.py
+++ sam_3km/sam_3km_evaluate/plot_maps_bias_srf_p99.py
@@ -20,7 +20,7 @@ dt = '2018-2021'
 path = '/marconi/home/userexternal/mdasilva'
 
 
-def import_obs(param, domain, dataset, season):
+def import_obs(param, domain, dataset):
 
 	arq   = '{0}/user/mdasilva/SAM-3km/post_evaluate/obs/p99_{1}_{2}_{3}_lonlat.nc'.format(path, domain, dataset, dt)	
 	data  = netCDF4.Dataset(arq)
@@ -32,7 +32,7 @@ def import_obs(param, domain, dataset, season):
 	return lat, lon, mean
 	
 		
-def import_cp_3km(param, domain, dataset, season):
+def import_cp_3km(param, domain, dataset):
 
 	arq   = '{0}/user/mdasilva/SAM-3km/post_evaluate/rcm/p99_{1}_{2}_{3}_lonlat.nc'.format(path, domain, dataset, dt)	
 	data  = netCDF4.Dataset(arq)
@@ -58,40 +58,36 @@ def basemap(lat, lon):
 	
 	
 # Import model and obs dataset
-lat, lon, cpc = import_obs('precip', domain, 'CPC', 'DJF')
+lat, lon, cpc = import_obs('precip', domain, 'CPC')
 
-lat, lon, era5 = import_obs('tp', domain, 'ERA5', 'DJF')
+lat, lon, era5 = import_obs('tp', domain, 'ERA5')
 
-lat, lon, regcm = import_cp_3km('pr', domain, 'RegCM5', 'DJF')
+lat, lon, regcm = import_cp_3km('pr', domain, 'RegCM5')
+
+mbe_regcm_cpc = regcm - cpc
+mbe_regcm_era5 = regcm - era5
 
 # Plot figure
 fig = plt.figure()   
-lev=np.arange(0, 100, 5)
-cor=cm.Blues
 font_size = 8
 	
-ax = fig.add_subplot(1, 3, 1)  
+ax = fig.add_subplot(2, 1, 1)  
 map, xx, yy = basemap(lat, lon)
-plt_map = map.contourf(xx, yy, cpc[0], levels=lev, cmap=cor, extend='both') 
-plt.title(u'(a) CPC', loc='left', fontsize=font_size, fontweight='bold')
+plt_map = map.contourf(xx, yy, mbe_regcm_cpc[0], levels=np.arange(-60, 70, 10), cmap=cm.BrBG, extend='both') 
+plt.title(u'(a) RegCM5 - CPC', loc='left', fontsize=font_size, fontweight='bold')
 
-ax = fig.add_subplot(1, 3, 2)  
+ax = fig.add_subplot(2, 1, 2)  
 map, xx, yy = basemap(lat, lon)
-plt_map = map.contourf(xx, yy, era5[0], levels=lev, cmap=cor, extend='both') 
-plt.title(u'(a) ERA5', loc='left', fontsize=font_size, fontweight='bold')
+plt_map = map.contourf(xx, yy, mbe_regcm_era5[0], levels=np.arange(-60, 70, 10), cmap=cm.BrBG, extend='both') 
+plt.title(u'(b) RegCM5 - ERA5', loc='left', fontsize=font_size, fontweight='bold')
 
-ax = fig.add_subplot(1, 3, 3)  
-map, xx, yy = basemap(lat, lon)
-plt_map = map.contourf(xx, yy, regcm[0], levels=lev, cmap=cor, extend='both') 
-plt.title(u'(c) RegCM5', loc='left', fontsize=font_size, fontweight='bold')
-
-cbar = plt.colorbar(plt_map, cax=fig.add_axes([0.84, 0.3, 0.03, 0.4]))
-cbar.set_label('Extreme wet precipitation (mm $^-$$^1$)', fontsize=font_size, fontweight='bold')
+cbar = plt.colorbar(plt_map, cax=fig.add_axes([0.75, 0.3, 0.03, 0.4]))
+cbar.set_label('Daily p99 (mm d$^-$$^1$)', fontsize=font_size, fontweight='bold')
 cbar.ax.tick_params(labelsize=font_size)
 
 # Path out to save figure
 path_out = '{0}/user/mdasilva/SAM-3km/figs/evaluate'.format(path)
-name_out = 'pyplt_maps_bias_{0}_CP-RegCM5_{1}_{2}.png'.format(var, domain, dt)
+name_out = 'pyplt_maps_bias_{0}_RegCM5_{1}_{2}.png'.format(var, domain, dt)
 plt.savefig(os.path.join(path_out, name_out), dpi=400, bbox_inches='tight')
 plt.show()
 exit()
