7eae7a00d65ed95773606fb758b6980b2108b6f3
Leidinice Silva <leidinicesilva@gmail.com>
2024-03-08 17:00:51 +0100

    update scripts

---
+++ sam_3km_cyclone/convert_csv2nc_stations.py
@@ -0,0 +1,196 @@
+# -*- coding:utf-8 -*-
+
+__author__      = "Leidinice Silva"
+__email__       = "leidinicesilva@gmail.com"
+__date__        = "Nov 20, 2023"
+__description__ = "This script convert .csv to .nc"
+
+import os
+import numpy as np
+import pandas as pd
+
+from netCDF4 import Dataset
+from dict_inmet_stations import inmet
+
+path = '/marconi/home/userexternal/mdasilva/OBS/BDMET/database'
+
+# choose variable: 2, 7, 8 and 21
+idx=2
+
+if idx == 2:
+	nc_var = 'pre'
+	unit_var = 'mm'
+	name_var = 'Hourly total of precipitation'
+	std_var = 'precipitation'
+elif idx == 7:
+	nc_var = 'rad'
+	unit_var = 'kJ.m**-2'
+	name_var = 'Hourly mean of solar radiation'
+	std_var = 'temperature'
+elif idx == 8:
+	nc_var = 'tmp'
+	unit_var = 'degrees C'
+	name_var = 'Hourly mean of air temperature'
+	std_var = 'temperature'
+else:
+	nc_var = 'uv'
+	unit_var = 'm.s**-1'
+	name_var = 'Hourly mean of wind speed'
+	std_var = 'wind'
+
+# create date list
+dt = pd.date_range('2018-01-01','2022-01-01', freq='H')
+dt = dt[:-1]
+
+for station in range(1, 567):
+	if station == 2:
+		continue
+	if station == 15:
+		continue
+	if station == 19:
+		continue
+	if station == 23:
+		continue
+	if station == 35:
+		continue
+	if station == 47:
+		continue
+	if station == 59:
+		continue
+	if station == 64:
+		continue
+	if station == 93:
+		continue
+	if station == 96:
+		continue
+	if station == 100:
+		continue
+	if station == 105:
+		continue
+	if station == 112:
+		continue
+	if station == 117:
+		continue
+	if station == 124:
+		continue
+	if station == 137:
+		continue
+	if station == 149:
+		continue
+	if station == 152:
+		continue
+	if station == 155:
+		continue
+	if station == 158:
+		continue
+	if station == 174:
+		continue
+	if station == 183:
+		continue
+	if station == 210:
+		continue
+	if station == 212:
+		continue
+	if station == 240:
+		continue
+	if station == 248:
+		continue
+	if station == 253:
+		continue
+	if station == 303:
+		continue
+	if station == 305:
+		continue
+	if station == 308:
+		continue
+	if station == 335:
+		continue
+	if station == 343:
+		continue
+	if station == 359:
+		continue
+	if station == 393:
+		continue
+	if station == 398:
+		continue
+	if station == 399:
+		continue
+	if station == 413:
+		continue
+	if station == 417:
+		continue
+	if station == 422:
+		continue
+	if station == 426:
+		continue
+	if station == 427:
+		continue
+	if station == 444:
+		continue
+	if station == 453:
+		continue
+	if station == 457:
+		continue
+	if station == 458:
+		continue
+	if station == 479:
+		continue
+	if station == 490:
+		continue
+	if station == 495:
+		continue
+	if station == 505:
+		continue
+	if station == 514:
+		continue
+	if station == 529:
+		continue
+	if station == 566:
+		continue
+													
+	print('Reading inmet station:', station, inmet[station][0])
+	# Reading smn station
+	data = pd.read_csv(os.path.join('{0}/csv/hourly/'.format(path), 'dados_{0}_H_2018-01-01_2021-12-31.csv'.format(inmet[station][0])), skiprows=9, encoding='ISO-8859-1', decimal=',', delimiter=';')
+	data_i = data.iloc[:, idx]
+	data_ii = data_i.replace(-999., np.nan)
+	data_values = np.array(data_ii, dtype=float)
+			
+	data_dates  = []
+	for i in range(len(dt)):
+			
+		data_dates.append('{0}'.format(dt[i]))
+		print('Date organized:', data_dates[i], data_values[i])
+		
+	nc_output = '{0}/nc/hourly/{1}/{1}_{2}_H_2018-01-01_2021-12-31.nc'.format(path, nc_var, inmet[station][0])
+
+	# create netcdf
+	ds = Dataset(nc_output, mode='w', format='NETCDF4_CLASSIC')
+
+	ds.Conventions 	= 'CF-1.6'
+	ds.title 	= 'Automatic Weather stations.'
+	ds.institution 	= 'Instituto Nacional de Meteorologia, INMET.'
+	ds.source 	= 'INMET Meteorological Database.'
+	ds.history 	= 'Rewrote via python script.'
+	ds.references 	= 'https://bdmep.inmet.gov.br/.'
+	ds.comment 	= 'This script convert .csv to .nc of weather station'
+		
+	ds.createDimension('time', None)
+
+	time 		= ds.createVariable('time', float, ('time'))
+	time.axis 	= 'L'
+	time.calendar 	= 'standard'
+	time.units	= 'Hours since {}'.format(data_dates[0])
+	time[:]		= range(len(data_dates))
+
+	var 		= ds.createVariable(nc_var,  float, ('time'))
+	var.units 	= unit_var
+	var.long_name 	= name_var
+	var.standard_name = std_var
+	var.missing_value = -999
+	var[:] 		= data_values[:]
+		
+	ds.close()
+	
+	if os.path.exists(nc_output): 
+		print('Done -->', nc_output)
+exit()
---
+++ sam_3km_cyclone/dict_inmet_stations.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+
+__author__      = "Leidinice Silva"
+__email__       = "leidinicesilva@gmail.com"
+__date__        = "Mar 08, 2024"
+__description__ = "Dictionary of the weather stations"
+
+"""
+Dictionary of the inmet automatic weather station 
+
+> {station}
+
+    Stations and locations, where:
+
+        1 - Automatic weather station: code, name, latitude, longitude, altitude
+
+    Examples:
+
+         1: ['A606', 'ARRAIAL_DO_CABO', -22.975278, -42.021389,	5.000000]
+"""
+
+inmet = {       1	:['A606', 'ARRAIAL_DO_CABO',	                     -22.975278,	-42.021389,	5.000000],
+		2	:['A619', 'PARATY',	                             -23.223611,	-44.726944,	3.000000],
+		3	:['A746', 'BARRA_DO_TURVO',	                     -24.962778,	-48.416389,	659.8900],
+		4	:['A771', 'SAO_PAULO-INTERLAGOS',	             -23.724501,	-46.677501,	771.0000],
+		5	:['A801', 'PORTO ALEGRE-JARDIM BOTANICO',	     -30.053611,	-51.174722,	41.18000],
+		6	:['A802', 'RIO_GRANDE',	                             -32.078889,	-52.167778,	4.920000],
+		7	:['A806', 'FLORIANOPOLIS',	                     -27.602500,	-48.620000,	4.870000],
+		8	:['A807', 'CURITIBA',	                             -25.448611,	-49.230556,	922.9100],
+		9	:['A834', 'TRAMANDAI',	                             -30.010278,	-50.135833,	4.560000],
+		10	:['A836', 'JAGUARA',	                             -32.534722,	-53.375833,	31.48000],
+		11	:['A837', 'SOLEDADE',	                             -28.859211,	-52.542387,	660.4400],
+		12	:['A853', 'CRUZ_ALTA',	                             -28.603440,	-53.673597,	426.6900],
+		13	:['A887', 'CAPAO_DO_LEAO-PELOTAS',	             -31.802500,	-52.407222,	13.00000]}
