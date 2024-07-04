53a11219c18766ec6735606cec5b24777909dad6
Leidinice Silva <leidinicesilva@gmail.com>
2024-07-04 16:43:32 +0200

    update scripts

--- sam_3km/sam_3km_cyclone/egu/plot_maps_precipitation_1hr_95th_ec.py
+++ sam_3km/sam_3km_cyclone/egu/plot_maps_precipitation_1hr_95th_ec.py
@@ -21,6 +21,7 @@ from dict_inmet_stations import inmet
 from datetime import datetime, timedelta
 from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 
+var = 'pr'
 font_size = 10
 path='/marconi/home/userexternal/mdasilva'
 
@@ -30,26 +31,16 @@ skip_list = [1,2,415,19,21,23,28,35,41,44,47,54,56,59,64,68,7793,100,105,106,107
 443,444,446,451,453,457,458,467,474,479,483,488,489,490,495,505,509,513,514,516,529,534,544,559,566]
 
 
-def remove_duplicates(date_list):
-	
-	unique_dates = []
-	for date in date_list:
-		if date not in unique_dates:
-			unique_dates.append(date)
-	
-	return unique_dates
-    
-
-def generate_daily_dates(start_date, end_date):
+def generate_hourly_dates(start_date, end_date):
     
 	dates = []
 	current_date = start_date
 	while current_date <= end_date:
-		dates.append(current_date.strftime('%Y%m%d'))
-		current_date += timedelta(days=1)
+		dates.append(current_date.strftime('%Y%m%d%H'))
+		current_date += timedelta(hours=6)
 	
 	return dates
-
+	
 
 def find_indices_in_date_list(date_list, target_dates):
     
@@ -64,20 +55,6 @@ def find_indices_in_date_list(date_list, target_dates):
 	return indices
 
 
-def select_days(dataset, indices):
-	
-	dataset_i = []
-	for idx_i in indices:
-		dataset_i.append(np.squeeze(dataset[idx_i,:,:]))
-	
-	dataset_ii = np.percentile(dataset_i, 95, axis=0)
-	print()
-	exit()
-	
-
-	return dataset_ii
-	
-	
 def read_dat_file(filename):
 
 	data = []
@@ -122,66 +99,30 @@ def open_dat_file(dataset):
 		
 		for j  in rows_list:
 			for k in j:
-				dt.append(str(k[0][:-2]))
-
+				dt.append(str(k[0][:]))
+				
 	return dt
 
 	
-def import_obs(param):
-
-	arq   = '{0}/user/mdasilva/SAM-3km/post_evaluate/obs/tp_SAM-3km_ERA5_day_2018-2021_lonlat.nc'.format(path)		
-	data  = netCDF4.Dataset(arq)
-	var   = data.variables[param][:] 
-	lat   = data.variables['lat'][:]	
-	lon   = data.variables['lon'][:]
-	mean = var[:][:,:,:]
-	
-	return lat, lon, mean
-	
-
-
-def import_sat(param):
-
-	arq   = '{0}/user/mdasilva/SAM-3km/post_cyclone/obs/gpm/precipitation_GPM-3BHHR_SAM-10km_day_20180101-20211231_lonlat.nc'.format(path)		
-	data  = netCDF4.Dataset(arq)
-	var   = data.variables[param][:] 
-	lat   = data.variables['lat'][:]	
-	lon   = data.variables['lon'][:]
-	mean = var[:][:,:,:]
-	
-	return lat, lon, mean
-	
-	
-def import_rcm(param):
-
-	arq   = '{0}/user/mdasilva/SAM-3km/post_evaluate/rcm/pr_SAM-3km_RegCM5_day_2018-2021_lonlat.nc'.format(path)	
-	data  = netCDF4.Dataset(arq)
-	var   = data.variables[param][:] 
-	lat   = data.variables['lat'][:]	
-	lon   = data.variables['lon'][:]
-	mean = var[:][:,:,:]
-	
-	return lat, lon, mean	
-
-
 def import_ws(param, indices):
 	
 	yy, xx, mean = [], [], [] 
-	for station in range(1, 567):
+	for station in range(1, 4): # 567	
 		if station in skip_list:
 			continue
 		if inmet[station][2] >= -11.25235:
 			continue
+		print(station)
 
 		yy.append(inmet[station][2])
 		xx.append(inmet[station][3])
 
-		arq  = xr.open_dataset('{0}/OBS/WS-SA/INMET/nc/hourly/{1}/'.format(path, param) + '{0}_{1}_H_2018-01-01_2021-12-31.nc'.format(param, inmet[station][0]))
+		arq  = xr.open_dataset('{0}/user/mdasilva/WS-SA/INMET/nc/hourly/{1}/'.format(path, param) + '{0}_{1}_H_2018-01-01_2021-12-31.nc'.format(param, inmet[station][0]))
 		data = arq[param]
 		time = data.sel(time=slice('2018-01-01','2021-12-31'))
-		var  = time.resample(time='1D').sum()
+		var  = time.resample(time='6H').sum()
 		var_ = var.values
-
+		
 		var_i = []
 		for idx_i in indices:
 			var_i.append(var_[idx_i])
@@ -189,35 +130,77 @@ def import_ws(param, indices):
 		mean.append(np.percentile(var_i, 95, axis=0))
 		
 	return yy, xx, mean
-	
-	
-# Generate list of daily dates from 2018 to 2021
-daily_dates = generate_daily_dates(datetime(2018, 1, 1), datetime(2021, 12, 31))
 
-# Import model and obs dataset 
-lat, lon, pr_era5 = import_obs('tp')
-lat, lon, pr_regcm5 = import_rcm('pr')
+
+def import_obs(param, dataset, indices):
+
+	arq = xr.open_dataset('{0}/user/mdasilva/SAM-3km/post_evaluate/obs/'.format(path) + '{0}_SAM-3km_{1}_1hr_2018-2021_lonlat.nc'.format(param, dataset))
+	data = arq[param]
+	time = data.sel(time=slice('2018-01-01','2021-12-31'))
+	var = time.resample(time='6H').sum()
+	lat = var.lat
+	lon = var.lon
+	var_ = var.values
+
+	var_i = []
+	for idx_i in indices:
+		var_i.append(np.squeeze(var_[idx_i,:,:]))
+	
+	mean = np.percentile(var_i, 95, axis=0)
+
+	
+	return lat, lon, mean
+
+
+def import_rcm(param, dataset, indices):
+
+	arq = xr.open_dataset('{0}/user/mdasilva/SAM-3km/post_evaluate/rcm/'.format(path) + '{0}_SAM-3km_{1}_1hr_2018-2021_lonlat.nc'.format(param, dataset))
+	data = arq[param]
+	time = data.sel(time=slice('2018-01-01','2021-12-31'))
+	var = time.resample(time='6H').sum()
+	lat = var.lat
+	lon = var.lon
+	var_ = var.values
+
+	var_i = []
+	for idx_i in indices:
+		var_i.append(np.squeeze(var_[idx_i,:,:]))
+	
+	mean = np.percentile(var_i, 95, axis=0)
+
+	
+	return lat, lon, mean
+	
+	
+def basemap(lat, lon):
+	
+	map = Basemap(projection='cyl', llcrnrlon=-80., llcrnrlat=-38., urcrnrlon=-34.,urcrnrlat=-8., resolution='c')
+	map.drawmeridians(np.arange(-80., -34., 12.), size=6, labels=[0,0,0,1], linewidth=0.4, color='black')
+	map.drawparallels(np.arange(-38., -8., 6.), size=6, labels=[1,0,0,0], linewidth=0.4, color='black')
+	map.readshapefile('{0}/github_projects/shp/shp_america_sul/america_sul'.format(path), 'america_sul', drawbounds=True, color='black')
+	
+	lons, lats = np.meshgrid(lon, lat)
+	xx, yy = map(lons, lats)
+	
+	return map, xx, yy
+		
+	
+# List of hourly dates 
+hourly_dates = generate_hourly_dates(datetime(2018, 1, 1, 0), datetime(2021, 12, 31, 23))
 
 # Import cyclone tracking date 
 dt_era5 = open_dat_file('ERA5')
 dt_regcm5 = open_dat_file('RegCM5')
 
-# Import indices after tracking
-era5_idx = remove_duplicates(dt_era5)
-era5_idx_i = find_indices_in_date_list(daily_dates, era5_idx)
-era5_idx_ii = select_days(pr_era5, era5_idx_i)
+# List of indices
+idx_era5 = find_indices_in_date_list(hourly_dates, dt_era5)
+idx_regcm = find_indices_in_date_list(hourly_dates, dt_regcm5)
 
-era5_idx_i = find_indices_in_date_list(daily_dates, era5_idx)
-era5_idx_ii = select_days(pr_era5, era5_idx_i)
-
-regcm5_idx = remove_duplicates(dt_regcm5)
-regcm5_idx_i = find_indices_in_date_list(daily_dates, regcm5_idx)
-regcm5_idx_ii = select_days(pr_regcm5, regcm5_idx_i)
-
-lat_i, lon_i, pr_inmet = import_ws('pre', era5_idx_i)
-
-lat_, lon_, pr_gpm = import_sat('precipitation')
-gpm_idx_ii = select_days(pr_gpm, era5_idx_i)
+# Import model and obs dataset 
+lat_i, lon_i, pr_inmet = import_ws('pre', idx_era5)
+lat, lon, pr_gpm = import_obs('precipitation', 'GPM', idx_era5)
+lat, lon, pr_era5 = import_obs('tp', 'ERA5', idx_era5)
+lat, lon, pr_regcm5 = import_rcm('pr', 'RegCM5', idx_era5)
 
 # Plot figure
 fig, (ax1, ax2, ax3) = plt.subplots(1,3, figsize=(14, 6), subplot_kw={"projection": ccrs.PlateCarree()})
@@ -237,7 +220,7 @@ ax1.coastlines()
 ax1.set_ylabel('Latitude',fontsize=font_size, fontweight='bold')
 ax1.set_xlabel('Longitude',fontsize=font_size, fontweight='bold')
 ax1.set_title('a) ERA5', loc='left', fontsize=font_size, fontweight='bold')
-cf = ax1.contourf(lon, lat, era5_idx_ii, levels=np.arange(0,84,4), transform=ccrs.PlateCarree(), extend='max', cmap=matplotlib.colors.ListedColormap(color))
+cf = ax1.contourf(lon, lat, pr_era5, levels=np.arange(0,20,1), transform=ccrs.PlateCarree(), extend='max', cmap=matplotlib.colors.ListedColormap(color))
 
 ax2.set_xticks(np.arange(-76,38.5,7), crs=ccrs.PlateCarree())
 ax2.set_yticks(np.arange(-34.5,15,5), crs=ccrs.PlateCarree())
@@ -249,7 +232,7 @@ ax2.add_feature(states_provinces, edgecolor='0.25')
 ax2.coastlines()
 ax2.set_xlabel('Longitude',fontsize=font_size, fontweight='bold')
 ax2.set_title('b) GPM', loc='left', fontsize=font_size, fontweight='bold')
-cf = ax2.contourf(lon_, lat_, gpm_idx_ii/3, levels=np.arange(0,84,4), transform=ccrs.PlateCarree(), extend='max', cmap=matplotlib.colors.ListedColormap(color))
+cf = ax2.contourf(lon, lat, pr_gpm, levels=np.arange(0,20,1), transform=ccrs.PlateCarree(), extend='max', cmap=matplotlib.colors.ListedColormap(color))
 cb = plt.colorbar(cf, cax=fig.add_axes([0.92, 0.3, 0.015, 0.4]))
 
 ax3.set_xticks(np.arange(-76,38.5,7), crs=ccrs.PlateCarree())
@@ -262,12 +245,13 @@ ax3.add_feature(states_provinces, edgecolor='0.25')
 ax3.coastlines()
 ax3.set_xlabel('Longitude',fontsize=font_size, fontweight='bold')
 ax3.set_title('c) RegCM5', loc='left', fontsize=font_size, fontweight='bold')
-cf = ax3.contourf(lon, lat, regcm5_idx_ii, levels=np.arange(0,84,4), transform=ccrs.PlateCarree(), extend='max', cmap=matplotlib.colors.ListedColormap(color))
-sc = ax3.scatter(lon_i, lat_i, 12, pr_inmet, cmap=matplotlib.colors.ListedColormap(color), edgecolors='black', linewidth=0.5, marker='o', vmin=0, vmax=80) 
+cf = ax3.contourf(lon, lat, pr_regcm5, levels=np.arange(0,20,1), transform=ccrs.PlateCarree(), extend='max', cmap=matplotlib.colors.ListedColormap(color))
+sc = ax3.scatter(lon_i, lat_i, 12, pr_inmet, cmap=matplotlib.colors.ListedColormap(color), edgecolors='black', linewidth=0.5, marker='o', vmin=0, vmax=20) 
 
 # Path out to save figure
 path_out = '{0}/user/mdasilva/SAM-3km/figs/cyclone'.format(path)
-name_out = 'pyplt_maps_95th_precipitation_EC_ERA5_RegCM5_SAM-3km_2018-2021.png'
+name_out = 'pyplt_maps_95th_hourly_precipitation_EC_ERA5_RegCM5_SAM-3km_2018-2021.png'
 plt.savefig(os.path.join(path_out, name_out), dpi=400, bbox_inches='tight')
 plt.show()
 exit()
+
