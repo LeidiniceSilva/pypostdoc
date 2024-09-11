4ed8231a458b6a8ea625ca3e122a215ed963dc1c
Leidinice Silva <leidinicesilva@gmail.com>
2024-09-11 17:53:40 +0200

    update scripts

--- cordex/plot_maps_bias_srf_freq_int.py
+++ cordex/plot_maps_bias_srf_freq_int.py
@@ -14,7 +14,8 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 from import_climate_tools import compute_mbe
 
-var = 'pr_int'
+var = 'pr'
+stats = 'int'
 freq = 'daily'
 domain = 'CSAM-3'
 idt, fdt = '2000', '2009'
@@ -29,7 +30,7 @@ def import_obs(param, domain, dataset, season):
 	else:
 		dt = '{0}_{1}-{2}'.format(season, idt, fdt)
 
-	arq   = '{0}/user/mdasilva/SAM-3km_v6/post/obs/{1}_int_{2}_{3}_{4}_lonlat.nc'.format(path, param, domain, dataset, dt)	
+	arq   = '{0}/user/mdasilva/CORDEX/post_evaluate/obs/{1}_{2}_{3}_{4}_{5}_lonlat.nc'.format(path, param, stats, domain, dataset, dt)	
 	data  = netCDF4.Dataset(arq)
 	var   = data.variables[param][:] 
 	lat   = data.variables['lat'][:]
@@ -42,11 +43,11 @@ def import_obs(param, domain, dataset, season):
 def import_rcm(param, domain, dataset, season):
 
 	if freq == 'hourly':
-		dt = '1hr_{0}_{1}-{2}_th0.5'.format(season, idt, fdt)
+		dt = '1hr_{0}_{1}-{2}_th0.5'.format(season, 2000, 2005)
 	else:
-		dt = '{0}_{1}-{2}'.format(season, idt, fdt)
+		dt = '{0}_{1}-{2}'.format(season, 2000, 2005)
 
-	arq   = '{0}/user/mdasilva/SAM-3km_v6/post/rcm/{1}_int_{2}_{3}_{4}_lonlat.nc'.format(path, param, domain, dataset, dt)	
+	arq   = '{0}/user/mdasilva/CORDEX/post_evaluate/rcm/{1}_{2}_{3}_{4}_{5}_lonlat.nc'.format(path, param, stats, domain, dataset, dt)	
 	data  = netCDF4.Dataset(arq)
 	var   = data.variables[param][:] 
 	lat   = data.variables['lat'][:]
@@ -71,10 +72,10 @@ def basemap(lat, lon):
 	
 # Import model and obs dataset
 if freq == 'hourly':
-	lat, lon, era5_djf = import_obs('tp', domain, 'ERA5', 'DJF')
-	lat, lon, era5_mam = import_obs('tp', domain, 'ERA5', 'MAM')
-	lat, lon, era5_jja = import_obs('tp', domain, 'ERA5', 'JJA')
-	lat, lon, era5_son = import_obs('tp', domain, 'ERA5', 'SON')
+	lat, lon, era5_djf = import_obs('pr', domain, 'ERA5', 'DJF')
+	lat, lon, era5_mam = import_obs('pr', domain, 'ERA5', 'MAM')
+	lat, lon, era5_jja = import_obs('pr', domain, 'ERA5', 'JJA')
+	lat, lon, era5_son = import_obs('pr', domain, 'ERA5', 'SON')
 
 	lat, lon, regcm_djf = import_rcm('pr', domain, 'RegCM5', 'DJF')
 	lat, lon, regcm_mam = import_rcm('pr', domain, 'RegCM5', 'MAM')
@@ -105,67 +106,86 @@ else:
 fig = plt.figure(figsize=(4, 8))   
 font_size = 8
 
-if freq == 'hourly':
-	levs = np.arange(-6, 6.5, 0.5)
-	legend = 'Intensity (mm h$^-$$^1$)'
-	dt = '1hr_{0}-{1}_th0.5'.format(idt, fdt)
+if stats == 'freq':
+	if freq == 'hourly':
+		levs = np.arange(-22, 23, 1)
+		legend = 'Frequency (%)'
+		dt = '1hr_{0}-{1}_th0.5'.format(idt, fdt)
+	else:
+		levs = np.arange(-44, 46, 2)
+		legend = 'Frequency (%)'
+		dt = '{0}-{1}'.format(idt, fdt)
+else:
+	if freq == 'hourly':
+		levs = np.arange(-6, 6.5, 0.5)
+		legend = 'Intensity (mm h$^-$$^1$)'
+		dt = '1hr_{0}-{1}_th0.5'.format(idt, fdt)
+	else:
+		levs = np.arange(-22, 23, 1)
+		legend = 'Intensity (mm d$^-$$^1$)'
+		dt = '{0}-{1}'.format(idt, fdt)
 
+if freq == 'hourly':
 	ax = fig.add_subplot(4, 1, 1)  
 	map, xx, yy = basemap(lat, lon)
 	plt_map = map.contourf(xx, yy, mbe_regcm_era5_djf[0][0], levels=levs, cmap=cm.BrBG, extend='neither') 
-	plt.title(u'(a) RegCM5-ERA5 DJF', loc='left', fontsize=font_size, fontweight='bold')
+	plt.title(u'(a) CPM3 - ERA5', loc='left', fontsize=font_size, fontweight='bold')
+	plt.ylabel(u'DJF', labelpad=20, fontsize=font_size, fontweight='bold')
 
 	ax = fig.add_subplot(4, 1, 2)  
 	map, xx, yy = basemap(lat, lon)
 	plt_map = map.contourf(xx, yy, mbe_regcm_era5_mam[0][0], levels=levs, cmap=cm.BrBG, extend='neither') 
-	plt.title(u'(b) RegCM5-ERA5 MAM', loc='left', fontsize=font_size, fontweight='bold')
+	plt.title(u'(b) CPM3 - ERA5', loc='left', fontsize=font_size, fontweight='bold')
+	plt.ylabel(u'MAM', labelpad=20, fontsize=font_size, fontweight='bold')
 
 	ax = fig.add_subplot(4, 1, 3)  
 	map, xx, yy = basemap(lat, lon)
 	plt_map = map.contourf(xx, yy, mbe_regcm_era5_jja[0][0], levels=levs, cmap=cm.BrBG, extend='neither') 
-	plt.title(u'(c) RegCM5-ERA5 JJA', loc='left', fontsize=font_size, fontweight='bold')
+	plt.title(u'(c) CPM3 - ERA5', loc='left', fontsize=font_size, fontweight='bold')
+	plt.ylabel(u'JJA', labelpad=20, fontsize=font_size, fontweight='bold')
 
 	ax = fig.add_subplot(4, 1, 4)  
 	map, xx, yy = basemap(lat, lon)
 	plt_map = map.contourf(xx, yy, mbe_regcm_era5_son[0][0], levels=levs, cmap=cm.BrBG, extend='neither') 
-	plt.title(u'(d) RegCM5-ERA5 SON', loc='left', fontsize=font_size, fontweight='bold')
+	plt.title(u'(d) CPM3 - ERA5', loc='left', fontsize=font_size, fontweight='bold')
+	plt.ylabel(u'SON', labelpad=20, fontsize=font_size, fontweight='bold')
 
 	cbar = plt.colorbar(plt_map, cax=fig.add_axes([0.84, 0.3, 0.03, 0.4]))
 	cbar.set_label('{0}'.format(legend), fontsize=font_size, fontweight='bold')
 	cbar.ax.tick_params(labelsize=font_size)
 	
-else:
-	levs = np.arange(-22, 23, 1)
-	legend = 'Intensity (mm d$^-$$^1$)'
-	dt = '{0}-{1}'.format(idt, fdt)
-	
+else:	
 	ax = fig.add_subplot(4, 1, 1)  
 	map, xx, yy = basemap(lat, lon)
 	plt_map = map.contourf(xx, yy, mbe_regcm_cpc_djf[0][0], levels=levs, cmap=cm.BrBG, extend='neither') 
-	plt.title(u'(a) RegCM5-CPC DJF', loc='left', fontsize=font_size, fontweight='bold')
+	plt.title(u'(a) CPM3 - ERA5', loc='left', fontsize=font_size, fontweight='bold')
+	plt.ylabel(u'DJF', labelpad=20, fontsize=font_size, fontweight='bold')
 
 	ax = fig.add_subplot(4, 1, 2)  
 	map, xx, yy = basemap(lat, lon)
 	plt_map = map.contourf(xx, yy, mbe_regcm_cpc_mam[0][0], levels=levs, cmap=cm.BrBG, extend='neither') 
-	plt.title(u'(b) RegCM5-CPC MAM', loc='left', fontsize=font_size, fontweight='bold')
+	plt.title(u'(b) CPM3 - ERA5', loc='left', fontsize=font_size, fontweight='bold')
+	plt.ylabel(u'MAM', labelpad=20, fontsize=font_size, fontweight='bold')
 
 	ax = fig.add_subplot(4, 1, 3)  
 	map, xx, yy = basemap(lat, lon)
 	plt_map = map.contourf(xx, yy, mbe_regcm_cpc_jja[0][0], levels=levs, cmap=cm.BrBG, extend='neither') 
-	plt.title(u'(c) RegCM5-CPC JJA', loc='left', fontsize=font_size, fontweight='bold')
+	plt.title(u'(c) CPM3 - ERA5', loc='left', fontsize=font_size, fontweight='bold')
+	plt.ylabel(u'JJA', labelpad=20, fontsize=font_size, fontweight='bold')
 
 	ax = fig.add_subplot(4, 1, 4)  
 	map, xx, yy = basemap(lat, lon)
 	plt_map = map.contourf(xx, yy, mbe_regcm_cpc_son[0][0], levels=levs, cmap=cm.BrBG, extend='neither') 
-	plt.title(u'(d) RegCM5-CPC SON', loc='left', fontsize=font_size, fontweight='bold')
+	plt.title(u'(d) CPM3 - ERA5', loc='left', fontsize=font_size, fontweight='bold')
+	plt.ylabel(u'SON', labelpad=20, fontsize=font_size, fontweight='bold')
 
 	cbar = plt.colorbar(plt_map, cax=fig.add_axes([0.84, 0.3, 0.03, 0.4]))
 	cbar.set_label('{0}'.format(legend), fontsize=font_size, fontweight='bold')
 	cbar.ax.tick_params(labelsize=font_size)
 
 # Path out to save figure
-path_out = '{0}/user/mdasilva/SAM-3km_v6/figs'.format(path)
-name_out = 'pyplt_maps_bias_{0}_{1}_RegCM5_{2}.png'.format(var, domain, dt)
+path_out = '{0}/user/mdasilva/CORDEX/figs/v1'.format(path)
+name_out = 'pyplt_maps_bias_{0}_{1}_{2}_RegCM5_{3}.png'.format(var, stats, domain, dt)
 plt.savefig(os.path.join(path_out, name_out), dpi=400, bbox_inches='tight')
 plt.show()
 exit()
