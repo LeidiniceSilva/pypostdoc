ad83a319ebfd5a049c2f4d0e251cb9656fbc9c1f
Maria Leidinice da Silva <leidinicesilva@gmail.com>
2025-03-05 16:31:10 +0100

    update scripts

--- cordex5/plot_maps_urban_mask.py
+++ cordex5/plot_maps_urban_mask.py
@@ -3,82 +3,95 @@
 __author__      = "Leidinice Silva"
 __email__       = "leidinicesilva@gmail.com"
 __date__        = "Nov 16, 2023"
-__description__ = "This script plot study area"
+__description__ = "This script plot urban mask"
 
 import os
-import sys
 import numpy as np
+import netCDF4 as nc
 import cartopy.crs as ccrs
-import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
+import cartopy.feature as cfeature
+import cartopy.mpl.ticker as cticker
 
-from cartopy import config
-from netCDF4 import Dataset as nc
-from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+# Load environment variables
+fname = "/leonardo/home/userexternal/mdasilva/leonardo_work/CORDEX5/ERA5/icbc/CSAM-3_CLM45_surface.nc"
+snum = os.getenv("snum")
+outp = "/leonardo/home/userexternal/mdasilva/leonardo_work/CORDEX5/figs" 
 
-# Specify directories 
-dirnc = '/leonardo/home/userexternal/mdasilva/leonardo_work/CORDEX5_v2/ERA5/icbc'
-domname = 'CSAM-3'
+# Read dataset
+f = nc.Dataset(fname)
+urb_2d = f.variables["urb_2d"][:]
+urb_frac = np.sum(urb_2d, axis=0)  # Summing over time (assuming time is first dimension)
 
-# RegCM file
-if len(sys.argv) > 1:
-    RCMf = nc(sys.argv[1], mode='r')
+lat = f.variables["xlat"][:]
+lon = f.variables["xlon"][:]
+
+# Convert longitude if necessary
+if snum in ["Australasia", "EastAsia"]:
+    lon = np.where(lon < 0, lon + 360, lon)
+
+# Check resolution
+rr = abs(lat[20, 20] - lat[20, 21])
+rr_03_diff = abs(rr - 0.03)
+rr_12_diff = abs(rr - 0.11)
+rr_25_diff = abs(rr - 0.22)
+
+# Determine urban mask threshold
+if rr_03_diff < rr_12_diff:
+    print("3 km resolution")
+    this_frac = 70
+elif rr_12_diff < rr_25_diff:
+    print("12 km resolution")
+    this_frac = 40
 else:
-    RCMf = nc(os.path.join(dirnc,domname+'_DOMAIN000.nc'), mode='r')
-    
-lat  = RCMf.variables['xlat'][:,:]
-lon  = RCMf.variables['xlon'][:,:]
-topo = RCMf.variables['topo'][:,:]
-lonc = RCMf.longitude_of_projection_origin
-latc = RCMf.latitude_of_projection_origin
-RCMf.close()
+    print("25 km resolution")
+    this_frac = 10
 
-# Creating mask of the border
-topo = topo[30:-30, 30:-30]
-border_mask = np.full((783, 1231), np.nan)
-border_mask[:29, :] = 1
-border_mask[-29:, :] = 1
-border_mask[:, :29] = 1
-border_mask[:, -29:] = 1
-x_mod = lon[30:-30,30:-30]
-y_mod = lat[30:-30,30:-30]
+# Create urban mask
+urb_mask = np.where(urb_frac > this_frac, 1, 0)
+urb_mask = np.where(np.isnan(urb_frac), np.nan, urb_mask)
 
-lon_bounds = [-85, -30]
-lat_bounds = [-60, 20]
-
-# Plot study area
-fig, ax = plt.subplots(subplot_kw={'projection': ccrs.PlateCarree()})
+# Plot both subplots in one figure
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 6), subplot_kw={'projection': ccrs.PlateCarree()})
 font_size = 10
 
-ax.contourf(lon, lat, border_mask, cmap='gray', levels=[0, 1])
-ax.text(-36, 11, u'\u25B2 \nN', color='black', fontsize=font_size, fontweight='bold')
-ax.plot(-47.9000, -16.0000, 'o', ms=4, markeredgewidth=0.75, color='white', mec='black') # Brazilia
-#ax.plot(-46.6396, -23.5558, 'o', ms=4, markeredgewidth=0.75, color='white', mec='black') # Sao Paulo
-ax.plot(-56.1674, -34.8335, 'o', ms=4, markeredgewidth=0.75, color='white', mec='black') # Montevideo
-ax.plot(-57.5759, -25.2637, 'o', ms=4, markeredgewidth=0.75, color='white', mec='black') # Asuncion
-ax.plot(-58.4004, -34.6051, 'o', ms=4, markeredgewidth=0.75, color='white', mec='black') # Buenos Aires
-ax.plot(-68.1193, -16.4897, 'o', ms=4, markeredgewidth=0.75, color='white', mec='black') # La Paz
-ax.plot(-70.6693, -33.4489, 'o', ms=4, markeredgewidth=0.75, color='white', mec='black') # Santiago
-
-# Add features
-ax.set_extent([lon_bounds[0], lon_bounds[1], lat_bounds[0], lat_bounds[1]], crs=ccrs.PlateCarree())
-ax.stock_img()
-ax.coastlines()
+# Urban Fraction Plot
+ax = axes[0]
+ax.set_extent([lon.min(), lon.max(), lat.min(), lat.max()])
+ax.add_feature(cfeature.COASTLINE, edgecolor='black')
 ax.add_feature(cfeature.BORDERS, linestyle=':')
+ax.set_title("(a) Urban Fraction", loc='left', fontsize=font_size, fontweight='bold')
+ax.text(-38, -38, u'\u25B2 \nN', color='black', fontsize=font_size, fontweight='bold')
+im = ax.contourf(lon, lat, urb_frac, levels=np.linspace(0, 100, 11), cmap="Blues", transform=ccrs.PlateCarree())
+cbar = plt.colorbar(im, ax=ax, orientation='horizontal')
+cbar.set_label('%')
 
-# Add gridlines with labels on right and bottom
-gridlines = ax.gridlines(draw_labels=True)
-gridlines.right_labels = False
-gridlines.bottom_labels = True
-gridlines.left_labels = True
-gridlines.top_labels = False
-gridlines.xlabel_style = {'size': 10}
-gridlines.ylabel_style = {'size': 10}
+gl = ax.gridlines(draw_labels=True, linestyle="-")
+gl.top_labels = False
+gl.right_labels = False
+gl.xformatter = cticker.LongitudeFormatter()
+gl.yformatter = cticker.LatitudeFormatter()
 
-# Path out to save figure
-path_out = '/leonardo/home/userexternal/mdasilva/leonardo_work/CORDEX5_v2/figs'
-name_out = 'pyplt_maps_study_area_RegCM5_CSAM-3_2000-2009.png'
-plt.savefig(os.path.join(path_out, name_out), dpi=400, bbox_inches='tight')
-plt.show()
-exit()
+# Urban Mask Plot
+ax = axes[1]
+ax.set_extent([lon.min(), lon.max(), lat.min(), lat.max()])
+ax.add_feature(cfeature.COASTLINE, edgecolor='black')
+ax.add_feature(cfeature.BORDERS, linestyle=':')
+ax.set_title(f"(b) Urban Mask (frac > {this_frac})", loc='left', fontsize=font_size, fontweight='bold')
+ax.text(-38, -38, u'\u25B2 \nN', color='black', fontsize=font_size, fontweight='bold')
+im = ax.contourf(lon, lat, urb_mask, levels=[0, 0.5, 1], cmap="Reds", transform=ccrs.PlateCarree())
+cbar = plt.colorbar(im, ax=ax, orientation='horizontal')
+cbar.set_label('%')
+
+gl = ax.gridlines(draw_labels=True, linestyle="-")
+gl.top_labels = False
+gl.right_labels = False
+gl.xformatter = cticker.LongitudeFormatter()
+gl.yformatter = cticker.LatitudeFormatter()
+
+# Save and close figure
+plt.tight_layout()
+os.makedirs(outp, exist_ok=True)
+plt.savefig(os.path.join(outp, "pyplt_maps_urban_mask_RegCM5_CSAM-3_2000-2009.png"), dpi=400, bbox_inches='tight')
+plt.close()
 
