c5c1d16a92cacab21a5713c3efc0da79433617f3
Maria Leidinice da Silva <leidinicesilva@gmail.com>
2025-05-18 19:35:14 +0200

    update scripts

--- sam_22/plot_maps_clim_srf.py
+++ sam_22/plot_maps_clim_srf.py
@@ -17,14 +17,19 @@ import cartopy.feature as cfeat
 from cartopy import config
 from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 
-var = 'clt'
+var = 'pr'
 obs = 'ERA5'
 dt = '1970-1971'
 domain = 'SAM-22'
 latlon = [-105, -16, -57, 18]
 
 exp_i = 'ctrl_RegCM5'
-exp_ii = 'vfqr_RegCM5'
+exp_i_tg = exp_i.split('_RegCM5')[0]
+exp_i_up = exp_i_tg.upper()
+
+exp_ii = 'rclcrit_RegCM5'
+exp_ii_tg = exp_ii.split('_RegCM5')[0]
+exp_ii_up = exp_ii_tg.upper()
 
 font_size = 8
 path = '/leonardo/home/userexternal/mdasilva/leonardo_work/{0}'.format(domain)
@@ -111,14 +116,14 @@ plot_data = {'Plot 1': {'data': obs_djf, 'title': '(a) {0} DJF'.format(obs)},
 'Plot 2': {'data': obs_mam, 'title': '(b) {0} MAM'.format(obs)},
 'Plot 3': {'data': obs_jja, 'title': '(c) {0} JJA'.format(obs)},
 'Plot 4': {'data': obs_son, 'title': '(d) {0} SON'.format(obs)},
-'Plot 5': {'data': exp_i_djf, 'title': '(e) CTRL DJF'},
-'Plot 6': {'data': exp_i_mam, 'title': '(f) CTRL MAM'},
-'Plot 7': {'data': exp_i_jja, 'title': '(g) CTRL JJA'},
-'Plot 8': {'data': exp_i_son, 'title': '(h) CTRL SON'},
-'Plot 9': {'data': exp_ii_djf, 'title': '(i) VFQR DJF'},
-'Plot 10': {'data': exp_ii_mam, 'title': '(j) VFQR MAM'},
-'Plot 11': {'data': exp_ii_jja, 'title': '(k) VFQR JJA'},
-'Plot 12': {'data': exp_ii_son, 'title': '(l) VFQR SON'}}
+'Plot 5': {'data': exp_i_djf, 'title': '(e) {0} DJF'.format(exp_i_up)},
+'Plot 6': {'data': exp_i_mam, 'title': '(f) {0} MAM'.format(exp_i_up)},
+'Plot 7': {'data': exp_i_jja, 'title': '(g) {0} JJA'.format(exp_i_up)},
+'Plot 8': {'data': exp_i_son, 'title': '(h) {0} SON'.format(exp_i_up)},
+'Plot 9': {'data': exp_ii_djf, 'title': '(i) {0} DJF'.format(exp_ii_up)},
+'Plot 10': {'data': exp_ii_mam, 'title': '(j) {0} MAM'.format(exp_ii_up)},
+'Plot 11': {'data': exp_ii_jja, 'title': '(k) {0} JJA'.format(exp_ii_up)},
+'Plot 12': {'data': exp_ii_son, 'title': '(l) {0} SON'.format(exp_ii_up)}}
 
 for ax, (key, value) in zip(axes, plot_data.items()):
 	data = value['data']
@@ -134,7 +139,7 @@ cbar.set_label('{0}'.format(dict_plot[var][0]), fontsize=font_size, fontweight='
 cbar.ax.tick_params(labelsize=font_size)
 	
 # Path out to save figure
-path_out = '{0}/figs/vfqr'.format(path)
+path_out = '{0}/figs/{1}'.format(path, exp_ii_tg)
 name_out = 'pyplt_maps_clim_{0}_{1}_RegCM5_{2}.png'.format(var, domain, dt)
 plt.savefig(os.path.join(path_out, name_out), dpi=400, bbox_inches='tight')
 plt.show()
